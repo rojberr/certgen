f7138c1611bc7554229f2707af7ce76797f04e53
Rojberr <rojberr@outlook.com>
2021-12-29 20:23:39 +0100

    refac(main): Refactor README.md and Main

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Unfortunately creating certificates in stem f java is a bit of a hassle. it's much easier to use external libraries.
+# CertGen
 
-It is easy to save and store certificates with use of keystore.
+This is a Java Certificate Generator tool.  
+
+![Photo by Ekrulila from Pexels](pexels-certificate-2292837.jpg)
+
+It is supposed to ease the pain of storing and creating certificates using Java,  
+because right now this may be a little of a hassle.
+
+It is easy to store and manage certificates using Java keystore tool, but this one helps
+in the process of creation for a user unskilled in programming.
+
+## Use
+
--- src/main/java/certgen/Main.java
+++ src/main/java/certgen/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         List<X509SelfSignedCert> certList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
@@ -18,28 +18,39 @@ public class Main {
             System.out.println("Welcome to 'List my Class'. Press:\n"
                     + "1 - Create a new cert and add it to the list\n"
                     + "2 - Show the actual list\n"
-                    + "3 - Delete\n"
-                    + "4 - Exit program");
+                    + "3 - Search for certificates\n"
+                    + "4 - Delete certificate with ID\n"
+                    + "5 - Exit program");
             String element = scanner.nextLine();
 
             switch (element) {
                 case "1":
                     certList.add(new X509SelfSignedCert());
+                    System.out.println("A certificate was added!\n");
                     break;
                 case "2":
+                    System.out.println("Here are all saved certificates with their properties: ");
                     System.out.println(certList);
+                    System.out.println();
                     break;
                 case "3":
-                    System.out.println("Which cert should be removed?");
-                    int deletionNumber = scanner.nextInt();
-                    if (deletionNumber <= certList.size()) {
-                        certList.remove(deletionNumber);
-                    } else {
-                        System.out.println("Please state a valid number withing the range of certificates list\n");
-                    }
+                    System.out.println("SEARCH\n");
                     break;
                 case "4":
-                    System.out.println("Goodbye!");
+                    System.out.println("Which cert should be removed?");
+                    int id = 0;
+                    try {
+                        id = scanner.nextInt();
+                        if (id > 0 && id < certList.size()) {
+                            throw new Exception();
+                        }
+                        certList.remove(id);
+                    } catch (Exception e) {
+                        System.out.println("Please state a valid number within the list!\n");
+                    }
+                    break;
+                case "5":
+                    System.out.println("Goodbye! EXIT\n");
                     break loop;
                 default:
                     System.out.println("Please type in a valid number! :)\n");
