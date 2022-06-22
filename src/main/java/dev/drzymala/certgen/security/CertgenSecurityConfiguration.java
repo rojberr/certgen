690fa4bf66aeb36f3e03a68f1d966f612453c0a8
Rojberr <rojberr@outlook.com>
2022-06-22 22:21:55 +0200

    :lock: feat(): Add Spring Security to the certgen

---
+++ src/main/java/dev/drzymala/certgen/security/CertgenSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package dev.drzymala.certgen.security;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+public class CertgenSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/generateX509", "/swagger-ui.html").permitAll()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+    }
+}
