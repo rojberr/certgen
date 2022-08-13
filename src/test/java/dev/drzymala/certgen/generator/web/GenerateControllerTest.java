968193bc95e5592ef553b11a9461189ae26f1b61
Rojberr <rojberr@outlook.com>
2022-08-13 17:49:25 +0200

    test(generate): Use new parameters and thus make test green

--- src/test/java/dev/drzymala/certgen/generator/web/GenerateControllerTest.java
+++ src/test/java/dev/drzymala/certgen/generator/web/GenerateControllerTest.java
@@ -22,13 +22,20 @@ class GenerateControllerTest {
 
     @Test
     public void createX509() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/generateX509?serialNumber=123321&keySize=2048&" +
-                                "issuer=CN+%3D+CAcertificate%2C+O+%3D+Test&validFrom=&validTill=&" +
-                                "subject=E+%3D+test-user1-without-subjaltname%40drzymala.devCN+%3D+Test+User+1+Without+SubjAltNameO+%3D" +
-                                "+Drzymala+Test&basicConstraints=Subject+Type%3DEnd+EntityPath+Length+Constraint%3DNone&" +
-                                "keyUsage=Digital+Signature%2C+Key+Encipherment+%28a0%29")
+        mockMvc.perform(MockMvcRequestBuilders.get("/generateX509" +
+                                "?version=3+%280x2%29" +
+                                "&serialNumber=234234234234" +
+                                "&signatureAlgorithm=SHA256withRSA" +
+                                "&keySize=2048" +
+                                "&issuer=CN+%3D+CAcertificate%2C+O+%3D+Test" +
+                                "&validFrom=" +
+                                "&validTill=" +
+                                "&subject=E+%3D+test-user1-without-subjaltname%40drzymala.dev" +
+                                        "CN+%3D+Test+User+1+Without+SubjAltNameO+%3D+Drzymala+Test" +
+                                "&basicConstraints=Subject+Type%3DEnd+EntityPath+Length+Constraint%3DNone" +
+                                "&keyUsage=Digital+Signature%2C+Key+Encipherment+%28a0%29")
                         .accept(MediaType.TEXT_PLAIN))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andReturn();
     }
 }
