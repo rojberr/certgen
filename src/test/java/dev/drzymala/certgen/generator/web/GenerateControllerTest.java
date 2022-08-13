70fe3888d8921096e016e7838090f947b9c9ba29
Rojberr <rojberr@outlook.com>
2022-08-13 17:29:30 +0200

    test(generator): Change test input to 'drz'

--- src/test/java/dev/drzymala/certgen/generator/web/GenerateControllerTest.java
+++ src/test/java/dev/drzymala/certgen/generator/web/GenerateControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,11 +23,12 @@ class GenerateControllerTest {
     @Test
     public void createX509() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/generateX509?serialNumber=123321&keySize=2048&" +
-                                "issuer=CN+%3D+RootCA%2C+O+%3D+Test&validFrom=&validTill=&" +
-                                "subject=E+%3D+test-user1-without-subjaltname%40bonelabs.comCN+%3D+Test+User+1+Without+SubjAltNameO+%3D" +
-                                "+Zertificon+Test&basicConstraints=Subject+Type%3DEnd+EntityPath+Length+Constraint%3DNone&" +
+                                "issuer=CN+%3D+CAcertificate%2C+O+%3D+Test&validFrom=&validTill=&" +
+                                "subject=E+%3D+test-user1-without-subjaltname%40drzymala.devCN+%3D+Test+User+1+Without+SubjAltNameO+%3D" +
+                                "+Drzymala+Test&basicConstraints=Subject+Type%3DEnd+EntityPath+Length+Constraint%3DNone&" +
                                 "keyUsage=Digital+Signature%2C+Key+Encipherment+%28a0%29")
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
                 .andReturn();
     }
 }
